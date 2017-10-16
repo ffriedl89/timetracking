e75a88eb3766af9186e4b61dcd6c2f324696676d
tomheller <office@thomasheller.net>
2017-10-16 20:06:02 +0200

    feat(logo): logo and app header

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,10 @@
 <template>
-  <header>
-    <h1>This is the new timetracking!</h1>
+  <header class="appheader">
+    <img class="logo" src="../assets/logo.svg" alt="">
+    <!-- <h1>This is the new timetracking!</h1>
     <p>Its much better than Excel!</p>
-    <btn text="TestBtn"></btn>
-    <login></login>
+    <btn text="TestBtn"></btn> -->
+    <login class="login"></login>
   </header>
 </template>
 
@@ -20,6 +21,21 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="css">
+  .appheader {
+    width: 100vw; 
+    height: 3.5rem;
+    background-color: #333;
+    border-bottom: 1px solid #373737;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+    box-shadow: rgba(0, 0, 0, 0.5) 0 2px 2px;
+  }
 
+  .logo {
+    width: 2.5rem;
+    margin: 0.5rem;
+    flex-basis: 2.5rem;
+  }
 </style>
